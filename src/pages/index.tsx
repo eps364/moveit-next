977b32839b85186a7a61f41c9af544304ab62e17
Emerson Pereira da Silva <pereiradasilva.emerson@gmail.com>
2021-02-23 22:50:40 -0300

    Initial Adjustments

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import { ExperienceBar } from "../components/ExperinceBar";
+
+export default function Home() {
+  return (
+    <div className="container">
+      <ExperienceBar />
+    </div>
+  )
+}
